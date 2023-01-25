1ae47a6d29482ccbf762e6cdf0043a6da4972c60
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-24 21:40:41 -0500

    print contents of a file

--- interpreter.c
+++ interpreter.c
@@ -8,10 +8,21 @@
 int main(int argc , char* argv[]) {
     if (argc != 2) {
         printf("Please provide ONE argument specifying the Brainfuck source code\n");
+        return 0;
     }
 
     printf(argv[1]);
     printf("\n");
+
+    FILE* file_ptr;
+    if ((file_ptr = fopen(argv[1], "r")) == NULL) {
+        printf("The file cannot be opened!");
+        return 0;
+    }
+
+    char c;
+    while ((c = fgetc(file_ptr)) != EOF)
+        printf("%c", c);
 
     return 0;
 }
