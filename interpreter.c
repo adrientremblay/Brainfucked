00db87d746cf6c71818c17ce3afb7e72169ffff0
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-24 21:24:27 -0500

    Print the file name

--- interpreter.c
+++ interpreter.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello World!\n");
+/*
+ This is a brain fuck interpreter written in C.
+ An interpreter is a program that runs and executes source code
+ in another language. It can thought of as a sort of pseudo-compiler.
+*/
+int main(int argc , char* argv[]) {
+    if (argc != 2) {
+        printf("Please provide ONE argument specifying the Brainfuck source code\n");
+    }
+
+    printf(argv[1]);
+    printf("\n");
+
+    return 0;
 }
