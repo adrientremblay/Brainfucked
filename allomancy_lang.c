76cb3f1d62c1f275584fec6879b522bfb50354e3
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-26 03:33:36 -0500

    first conception of what each char would do in allomancy language

---
+++ allomancy_lang.c
@@ -0,0 +1,24 @@
+/*
+ALLOMANCY LANG
+
+---non-metals---
+> face right
+< face left
++ increase cur val
+- decrease cur val
+
+---physical-metals---
+s(teel) move forward 
+i(ron) move backwards
+t(in) output char
+p(ewter) increases value of + and -
+
+---mental-metals---
+b(ronze) if 0 face left, otherwise face right
+c(opper) decreases value of + and -
+z(inc) *p doubled
+b(rass)*p halved
+
+---higher-metals---
+
+*/
